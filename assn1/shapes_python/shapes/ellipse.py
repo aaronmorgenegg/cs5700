2c0689514f8ef52e2edb3b1995355f4622920c5e
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-08 18:56:27 -0600

    Refactored ellipse construction and foci, added scale method

--- assn1/shapes_python/shapes/ellipse.py
+++ assn1/shapes_python/shapes/ellipse.py
@@ -25,25 +25,48 @@ class Ellipse:
 
     def __constructWithCoords(x1, y1, x2, y2, x3, y3):
         self.__center = Point(x1, y1)
-        self.__focus1 = Point(x2, y2)
-        self.__focus2 = Point(x3, y3)
+        focus1 = Point(x2, y2)
+        focus2 = Point(x3, y3)
+        mirror1 = __getMirrorFocus(self.__center, focus1)
+        mirror2 = __getMirrorFocus(self.__center, focus2)
+        self.__foci1 = (focus1, mirror1)
+        self.__foci2 = (focus2, mirror2)
 
     def __constructWithPoints(point1, point2, point3):
         self.__center = point1
-        self.__focus1 = point2
-        self.__focus2 = point3
+        focus1 = point2
+        focus2 = point3
+        mirror1 = __getMirrorFocus(self.__center, focus1)
+        mirror2 = __getMirrorFocus(self.__center, focus2)
+        self.__foci1 = (focus1, mirror1)
+        self.__foci2 = (focus2, mirror2)
+
+    def __getMirrorFocus(center, focus):
+        dx = focus.x - center.x
+        dy = focus.y - center.y
+        x = focus.x - dx*2
+        y = focus.y - dy*2
+        return Point(x, y)
 
     @property
     def center(self):
         return self.__center
 
     @property
+    def foci1(self):
+        return self.__foci1
+
+    @property
+    def foci2(self):
+        return self.__foci2
+
+    @property
     def focus1(self):
-        return self.__focus1
+        return self.__foci1[0]
 
     @property
     def focus2(self):
-        return self.__focus2
+        return self.__foci2[0]
 
     def move(deltaX, deltaY):
         self.__center.move(deltaX, deltaY)
@@ -52,4 +75,15 @@ class Ellipse:
 
     def computeArea():
         pass # TODO
+
+    def scale(scaleFactor):
+        Validator.validatePositiveDouble(scaleFactor())
+        f1_dx = self.__focus1.x - self.__center.x
+        f1_dy = self.__focus1.y - self.__center.y
+        f2_dx = self.__focus2.x - self.__center.x
+        f2_dy = self.__focus2.y - self.__center.y
+        self.__focus1.x = f1_dx*scaleFactor + self.__center.x
+        self.__focus1.y = f1_dy*scaleFactor + self.__center.y
+        self.__focus2.x = f2_dx*scaleFactor + self.__center.x
+        self.__focus2.y = f2_dy*scaleFactor + self.__center.y
 
