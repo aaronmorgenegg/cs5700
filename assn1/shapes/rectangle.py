9f84009bbacf55706b917f28609b8e296c2bdef6
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-08 18:25:14 -0600

    Bug fixes for arg passing

--- assn1/shapes/rectangle.py
+++ assn1/shapes/rectangle.py
@@ -15,26 +15,26 @@ class Rectangle:
             self.__constructWithCoords(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
         except ShapeException:
             pass
-        try: # Construct with point1, point2, point3
+        try: # Construct with point1, point2, point3, point4
             self.__constructWithPoints(args[0], args[1], args[2], args[3])
         except ShapeException:
             pass
-        # Construct with line1, line2, line3
+        # Construct with line1, line2, line3, line4
         self.__constructWithLines(args[0], args[1], args[2], args[3])
 
         Validator.validateRectangle(self, "Rectangle is invalid")
 
     def __constructWithCoords(x1, y1, x2, y2, x3, y3, x4, y4):
-        self.__line1 = Line(x1=x1, y1=y1, x2=x2, y2=y2)
-        self.__line2 = Line(x1=x2, y1=y2, x2=x3, y2=y3)
-        self.__line3 = Line(x1=x3, y1=y3, x2=x4, y2=y4)
-        self.__line4 = Line(x1=x4, y1=y4, x2=x1, y2=y1)
+        self.__line1 = Line(x1, y1, x2, y2)
+        self.__line2 = Line(x2, y2, x3, y3)
+        self.__line3 = Line(x3, y3, x4, y4)
+        self.__line4 = Line(x4, y4, x1, y1)
 
     def __constructWithPoints(point1, point2, point3, point4):
-        self.__line1 = Line(point1=point1, point2=point2)
-        self.__line2 = Line(point1=point2, point2=point3)
-        self.__line3 = Line(point1=point3, point2=point4)
-        self.__line4 = Line(point1=point4, point2=point1)
+        self.__line1 = Line(point1, point2)
+        self.__line2 = Line(point2, point3)
+        self.__line3 = Line(point3, point4)
+        self.__line4 = Line(point4, point1)
 
     def __constructWithLines(line1, line2, line3, line4):
         self.__line1 = line1
--- assn1/shapes/triangle.py
+++ assn1/shapes/triangle.py
@@ -25,14 +25,14 @@ class Triangle:
         Validator.validateTriangle(self, "Triangle is invalid")
 
     def __constructWithCoords(x1, y1, x2, y2, x3, y3):
-        self.__line1 = Line(x1=x1, y1=y1, x2=x2, y2=y2)
-        self.__line2 = Line(x1=x2, y1=y2, x2=x3, y2=y3)
-        self.__line3 = Line(x1=x3, y1=y3, x2=x1, y2=y1)
+        self.__line1 = Line(x1, y1, x2, y2)
+        self.__line2 = Line(x2, y2, x3, y3)
+        self.__line3 = Line(x3, y3, x1, y1)
 
     def __constructWithPoints(point1, point2, point3):
-        self.__line1 = Line(point1=point1, point2=point2)
-        self.__line2 = Line(point1=point2, point2=point3)
-        self.__line3 = Line(point1=point3, point2=point1)
+        self.__line1 = Line(point1, point2)
+        self.__line2 = Line(point2, point3)
+        self.__line3 = Line(point3, point1)
 
     def __constructWithLines(line1, line2, line3):
         self.__line1 = line1
