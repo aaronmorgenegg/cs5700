241ebb7e5976a7d6ec0d2809fce32839c827521b
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-10 13:35:23 -0600

    Added validator tests

---
+++ assn1/shapes_python/tests/test_validator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from shapes.shape_exception import ShapeException
+from shapes.validator import Validator, isInfinite
+
+
+class TestValidator(unittest.TestCase):
+    def testIsInfinite(self):
+        self.assertTrue(isInfinite(float('inf')))
+        self.assertTrue(isInfinite(float('-inf')))
+        self.assertFalse(isInfinite(0))
+        self.assertFalse(isInfinite(1231245513632623.2134))
+        self.assertFalse(isInfinite(-1234125125124512.2345))
+
+    def testValidateDouble(self):
+        Validator.validateDouble(123.456, "Double unexpectedly invalid")
+        Validator.validateDouble(0, "Double unexpectedly invalid")
+        Validator.validateDouble(-123.456, "Double unexpectedly invalid")
+
+        self.assertRaises(ShapeException, Validator.validateDouble, None, "None is not a valid double")
+        self.assertRaises(ShapeException, Validator.validateDouble, float('inf'), "Inf is not a valid double")
+        self.assertRaises(ShapeException, Validator.validateDouble, float('-inf'), "-Inf is not a valid double")
+        self.assertRaises(ShapeException, Validator.validateDouble, "foo", "String \'foo\' is not a valid double")
+
+    def testValidatePositiveDouble(self):
+        Validator.validatePositiveDouble(123.456, "Double unexpectedly invalid")
+        Validator.validatePositiveDouble(0, "Double unexpectedly invalid")
+
+        self.assertRaises(ShapeException, Validator.validatePositiveDouble, None, "None is not a valid positive double")
+        self.assertRaises(ShapeException, Validator.validatePositiveDouble, float('inf'), "Inf is not a valid positive double")
+        self.assertRaises(ShapeException, Validator.validatePositiveDouble, float('-inf'), "-Inf is not a valid positive double")
+        self.assertRaises(ShapeException, Validator.validatePositiveDouble, "foo", "String \'foo\' is not a valid positive double")
+        self.assertRaises(ShapeException, Validator.validatePositiveDouble, -123.456, "-123.456 is not a valid positive double")
