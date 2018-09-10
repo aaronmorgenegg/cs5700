cda6f0d45088da09d0efeae195c449e700c27a6e
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-09 19:15:17 -0600

    Added triangle construction tests

---
+++ assn1/shapes_python/tests/test_triangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from shapes.line import Line
+from shapes.point import Point
+from shapes.shape_exception import ShapeException
+from shapes.triangle import Triangle
+
+
+class TestTriangle(unittest.TestCase):
+    def testValidConstruction(self):
+        p1 = Point(1, 2)
+        p2 = Point(1, 1)
+        p3 = Point(2, 1)
+        t1 = Triangle(p1, p2, p3)
+
+        self.assertEqual(p1, t1.point1)
+        self.assertEqual(p2, t1.point2)
+        self.assertEqual(p3, t1.point3)
+
+        l1 = Line(4.33, 8, -4, 3.67)
+        l2 = Line(-4, 3.67, 2.19, -5)
+        l3 = Line(2.19, -5, 4.33, 8)
+        t2 = Triangle(l1, l2, l3)
+
+        self.assertEqual(l1, t2.line1)
+        self.assertEqual(l2, t2.line2)
+        self.assertEqual(l3, t2.line3)
+
+        t3 = Triangle(1, 2, 5, 1, 3, 3)
+        self.assertEqual(1, t3.point1.x)
+        self.assertEqual(2, t3.point1.y)
+        self.assertEqual(5, t3.point2.x)
+        self.assertEqual(1, t3.point2.y)
+        self.assertEqual(3, t3.point3.x)
+        self.assertEqual(3, t3.point3.y)
+
+    def testInvalidConstruction(self):
+        p1 = Point(1, 2)
+        p2 = Point(1, 1)
+        p3 = Point(1, 3)
+        self.assertRaises(ShapeException, Triangle, p1, p2, p3)
+
+        l1 = Line(4.33, 8, -4, 3.67)
+        l2 = Line(-4, 3.67, 2.19, -5)
+        l3 = Line(2.19, -5, 4.33, 7)
+        self.assertRaises(ShapeException, Triangle, l1, l2, l3)
+
+        l4 = Line(1, 1, 1, 4)
+        l5 = Line(2, 1, 2, 5)
+        l6 = Line(3, 1, 3, 6)
+        self.assertRaises(ShapeException, Triangle, l4, l5, l6)
+
+    # def testMove(self):
+    #     l1 = Line(1, 2, 4, 10)
+    #
+    #     l1.move(3, 4)
+    #     self.assertEqual(4, l1.point1.x)
+    #     self.assertEqual(6, l1.point1.y)
+    #     self.assertEqual(7, l1.point2.x)
+    #     self.assertEqual(14, l1.point2.y)
+    #
+    #     l1.move(.4321, .7654)
+    #     self.assertEqual(4.4321, l1.point1.x)
+    #     self.assertEqual(6.7654, l1.point1.y)
+    #     self.assertEqual(7.4321, l1.point2.x)
+    #     self.assertEqual(14.7654, l1.point2.y)
+    #
+    #     l1.move(-.4321, -.7654)
+    #     self.assertEqual(4, l1.point1.x)
+    #     self.assertEqual(6, l1.point1.y)
+    #     self.assertEqual(7, l1.point2.x)
+    #     self.assertEqual(14, l1.point2.y)
