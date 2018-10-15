12321d8e994394ee8a70b386e5bafa90a3fa8b05
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-15 14:55:03 -0600

    Refactor image class to inherit from rectangle, added some image tests

--- assn3/shapes_python/shapes/image.py
+++ assn3/shapes_python/shapes/image.py
@@ -6,50 +6,27 @@ Image Shape
 This class represents embedded image shapes that contain an image.
 Users of an image shape can also compute its area
 """
-from shapes.line import Line
-from shapes.shape import Shape
+from shapes.rectangle import Rectangle
 from shapes.shape_exception import ShapeException
 from shapes.validator import Validator
 
 
-class Image(Shape):
+class Image(Rectangle):
     def __init__(self, *args, **kwargs):
-        self.center = args[0]
-        self.points = [args[1], args[2]]
-        self.source = args[3]
+        self.source = args[-1]
+        del args[-1]
+        super().__init__(args, kwargs)
         Image.validateImage(self, "Image is invalid")
 
     @staticmethod
     def validateImage(value, errorMessage):
         if not isinstance(value, Image):
-            raise ShapeException(errorMessage)
-
-        value.center.validate()
-        value.points[0].validate()
-        value.points[1].validate()
-        Line(value.points[0], value.points[1])
+            raise ShapeException(errorMessage)\
 
         Validator.validateSource(value.source)
 
     def validate(self):
         return Image.validateImage(self, "Image is invalid")
-
-    @property
-    def point1(self):
-        return self.points[0]
-
-    @property
-    def point2(self):
-        return self.points[1]
-
-    def computeWidth(self):
-        return self.point2.x - self.point1.x
-
-    def computeHeight(self):
-        return self.point1.y - self.point2.y
-
-    def computeArea(self):
-        return self.computeWidth() * self.computeHeight()
 
     def toString(self, name="image"):
         return super().toString(name) + ", {}".format(self.source)
---
+++ assn3/shapes_python/tests/test_image.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from shapes.image import Image
+from shapes.point import Point
+from shapes.shape_exception import ShapeException
+from shapes.shape_factory import ShapeFactory
+
+
+class TestImage(unittest.TestCase):
+    def testValidateImage(self):
+        i1 = ShapeFactory.build("image", Point(1, 1), Point(4, 1), Point(4, 3), Point(1, 3), "images/red_eyed_tree_frog.png")
+        Image.validateImage(i1, "Image unexpectedly invalid")
+
+        self.assertRaises(ShapeException, Image.validateImage, "(1, 1, 4, 1, 4, 3, 1, 3, images/red_eyed_tree_frog.png)",
+                          "String \'(1, 1, 4, 1, 4, 3, 1, 3, images/red_eyed_tree_frog.png)\' is not a valid embedded image")
+        self.assertRaises(ShapeException, Image.validateImage, Point(1, 1), "Point is not a valid embedded image")
+
+    def testValidConstruction(self):
+        p1 = Point(1, 3)
+        p2 = Point(5, 5)
+        p3 = Point(5, 1)
+        p4 = Point(1, 1)
+        s1 = "images/red_eyed_tree_frog.png"
+        i1 = ShapeFactory.build("image", p1, p2, p3, p4, s1)
+
+        self.assertEqual(p1, i1.point1)
+        self.assertEqual(p2, i1.point2)
+        self.assertEqual(s1, i1.source)
+
+    def testInvalidConstruction(self):
+        p1 = Point(1, 3)
+        p2 = Point(5, 5)
+        p3 = Point(5, 1)
+        p4 = Point(1, 1)
+        s1 = "images/red_eyed_tree_frog.png"
+        self.assertRaises(ShapeException, ShapeFactory.build, "image", p1, p1, p4, p4, s1)
+
+        s2 = "images/red_eyed_tree_frog.txt"
+        self.assertRaises(ShapeException, ShapeFactory.build, "image", p1, p2, p3, p4, s2)
