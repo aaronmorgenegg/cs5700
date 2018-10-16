f06e1995456ccf763cd80c77e6458cd05744c61d
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-16 16:08:53 -0600

    Added a lot more tests for composite shapes

--- assn3/shapes_python/tests/test_composite.py
+++ assn3/shapes_python/tests/test_composite.py
@@ -53,19 +53,85 @@ class TestRectangle(unittest.TestCase):
         pass # TODO
 
     def testComputeArea(self):
-        pass # TODO
+        center = Point(0, 0)
+        r1 = ShapeFactory.build("rectangle", Point(1, 1), Point(4, 1), Point(4, 3), Point(1, 3))
+        t1 = ShapeFactory.build("triangle", Point(1, 2), Point(5, 1), Point(3, 3))
+        c1 = ShapeFactory.build("circle", Point(0, 0), Point(2, 0), Point(0, 2))
+        e1 = ShapeFactory.build("ellipse", Point(0, 0), Point(3, 0), Point(0, 2))
+        l1 = ShapeFactory.build("line", Point(4, 2), Point(6, 1))
+        cm1 = ShapeFactory.build("composite", center, r1)
+        expected = r1.computeArea()
+        actual = cm1.computeArea()
+        self.assertEqual(expected, actual)
+
+        cm2 = ShapeFactory.build("composite", center, t1, c1)
+        expected = t1.computeArea() + c1.computeArea()
+        actual = cm2.computeArea()
+        self.assertEqual(expected, actual)
+
+        cm3 = ShapeFactory.build("composite", center, cm1, e1, cm2)
+        expected = cm1.computeArea() + e1.computeArea() + cm2.computeArea()
+        actual = cm3.computeArea()
+        self.assertEqual(expected, actual)
+
+        cm4 = ShapeFactory.build("composite", center)
+        expected = 0
+        actual = cm4.computeArea()
+        self.assertEqual(expected, actual)
+
+        cm5 = ShapeFactory.build("composite", center, l1, r1)
+        expected = r1.computeArea()
+        actual = cm5.computeArea()
+        self.assertEqual(expected, actual)
 
     def testAddShape(self):
-        pass # TODO
+        center = Point(0, 0)
+        cm1 = ShapeFactory.build("composite", center)
+        self.assertEqual(0, len(cm1.shapes))
+        r1 = ShapeFactory.build("rectangle", Point(1, 1), Point(4, 1), Point(4, 3), Point(1, 3))
+        t1 = ShapeFactory.build("triangle", Point(1, 2), Point(5, 1), Point(3, 3))
+        cm1.addShape(r1)
+        self.assertEqual(cm1.getShape(0), r1)
+        cm1.addShape(t1)
+        self.assertEqual(cm1.getShape(1), t1)
 
     def testRemoveshape(self):
-        pass # TODO
+        center = Point(0, 0)
+        t1 = ShapeFactory.build("triangle", Point(1, 2), Point(5, 1), Point(3, 3))
+        c1 = ShapeFactory.build("circle", Point(0, 0), Point(2, 0), Point(0, 2))
+        cm1 = ShapeFactory.build("composite", center, t1, c1)
+        self.assertEqual(cm1.getShape(0), t1)
+        self.assertEqual(cm1.getShape(1), c1)
+
+        cm1.removeShape(1)
+        self.assertEqual(cm1.getShape(0), t1)
+        self.assertEqual(len(cm1.shapes), 1)
+
+        cm1.removeShape(0)
+        self.assertEqual(len(cm1.shapes), 0)
+
+        self.assertRaises(ShapeException, cm1.removeShape, 3)
 
     def testClearShapes(self):
-        pass # TODO
+        center = Point(0, 0)
+        t1 = ShapeFactory.build("triangle", Point(1, 2), Point(5, 1), Point(3, 3))
+        c1 = ShapeFactory.build("circle", Point(0, 0), Point(2, 0), Point(0, 2))
+        cm1 = ShapeFactory.build("composite", center, t1, c1)
+        self.assertEqual(cm1.getShape(0), t1)
+        self.assertEqual(cm1.getShape(1), c1)
+        self.assertEqual(len(cm1.shapes), 2)
+
+        cm1.clearShapes()
+        self.assertEqual(len(cm1.shapes), 0)
 
     def testGetShape(self):
-        pass # TODO
+        center = Point(0, 0)
+        t1 = ShapeFactory.build("triangle", Point(1, 2), Point(5, 1), Point(3, 3))
+        c1 = ShapeFactory.build("circle", Point(0, 0), Point(2, 0), Point(0, 2))
+        cm1 = ShapeFactory.build("composite", center, t1, c1)
+        self.assertEqual(cm1.getShape(0), t1)
+        self.assertEqual(cm1.getShape(1), c1)
+        self.assertRaises(ShapeException, cm1.getShape, 2)
 
     def testToString(self):
         center = Point(0,0)
