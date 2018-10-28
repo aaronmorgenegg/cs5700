8c723c8866a14d9c4b7cb7f12566aefd03976446
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-28 17:47:31 -0600

    Creating sudoku board construction tests, testing blocks

--- assn4/sudoku/tests/test_puzzle_reader.py
+++ assn4/sudoku/tests/test_puzzle_reader.py
@@ -6,7 +6,7 @@ from sudoku_solver.puzzle_reader import PuzzleReader
 class TestPuzzleReader(unittest.TestCase):
     def testReadFromFile(self):
         test_reader = PuzzleReader()
-        filename = "test_puzzles/input/Puzzle-9x9-0001.txt"
+        filename = "test_puzzles/test_input/Puzzle-9x9-0001.txt"
         SudokuBoard = test_reader.readPuzzle(filename)
         self.assertEqual(SudokuBoard.size, 9)
         self.assertEqual(SudokuBoard.valid_symbols, ['1','2','3','4','5','6','7','8','9'])
