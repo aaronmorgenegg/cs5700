e211c8472822469a8613d6ce31d428ae2dab0373
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-09 18:24:39 -0700

    Dynamically choosing strategies, added more time tracking

--- assn4/sudoku/sudoku_solver/sudoku_solver.py
+++ assn4/sudoku/sudoku_solver/sudoku_solver.py
@@ -6,25 +6,32 @@ from sudoku_solver.timer import Timer
 class SudokuSolver:
     def __init__(self, sudoku_board):
         self.sudoku_board = sudoku_board
-        self.timer = Timer()
+        self.strategies = [OnlyChoice()]
+        self.time = {'total': 0, 'choosing_strategy': 0, 'applying_strategy': 0}
 
     def solvePuzzle(self):
-        self.timer.startTimer()
+        total_timer = Timer()
+        total_timer.startTimer()
 
         try:
             self.sudoku_board.validate()
         except SudokuBoardException as e:
             return self._invalidSolutionToString(e)
 
-        # TODO: implement strategies to solve sudoku puzzle
-        max_iter = 100
-        while self.sudoku_board.num_blank_cells > 0 and max_iter >= 0:
-            strategy = OnlyChoice()
-            strategy.invoke(self.sudoku_board)
-            max_iter -= 1
-            if max_iter == 0:
-                print("Max iterations achieved")
+        while self.sudoku_board.num_blank_cells > 0:
+            solve_timer = Timer()
+            solve_timer.startTimer()
+            for strategy in self.strategies:
+                if strategy.isAppropriate(self.sudoku_board):
+                    self.time['choosing_strategy'] += solve_timer.stopTimer()
+                    solve_timer.startTimer()
+                    strategy.invoke(self.sudoku_board)
+                    self.time['applying_strategy'] += solve_timer.stopTimer()
+                    break
+            # This means none of the strategies worked
+            return self._invalidSolutionToString("No strategy could be found to solve this puzzle")
 
+        self.time['total'] += total_timer.stopTimer()
         return self._solutionToString()
 
     def _invalidSolutionToString(self, error):
@@ -32,5 +39,7 @@ class SudokuSolver:
 
     def _solutionToString(self):
         string = self.sudoku_board.toString()
-        string += "\nTotal time: " + str(self.timer.stopTimer())
+        string += "\nTotal time               : " + str(self.time['total'])
+        string += "\nChoosing Strategies time : " + str(self.time['choosing_strategy'])
+        string += "\nApplying Strategies time : " + str(self.time['applying_strategy'])
         return string
